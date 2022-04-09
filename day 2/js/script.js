3ecb30336823ec0ffec13a1fa9a1e5aafe57c278
Lucas 07 <lucas.rgrecco@gmail.com>
2022-04-09 11:46:13 -0300

    day 2

--- day 2/js/script.js
+++ day 2/js/script.js
@@ -1,12 +1,12 @@
 
-    //variáveis //
-    const userNome = document.getElementById('nome').string;
-    const userIdade = document.getElementById('idade').string;
-    const userLinguagem = document.getElementById('linguagem').string;
-    const msgContainer = document.getElementById('mensagem');
-    const btnEnviar = document.getElementById('enviar');
-
     function mostrarMensagem(){
-       msgContainer.innerHTML = `Olá ${userNome}, você tem ${userIdade} anos e já está aprendendo ${userLinguagem} ! ` 
+        const userNome = document.getElementById('nome').string;
+        const userIdade = document.getElementById('idade').string;
+        const userLinguagem = document.getElementById('linguagem').string;
+        const msgContainer = document.getElementById('mensagem');
+        const btnEnviar = document.getElementById('enviar');
+
+        msgContainer.innerHTML = `Olá ${userNome}, você tem ${userIdade} anos e já está aprendendo ${userLinguagem} ! ` 
+        btnEnviar.addEventListener("click", mostrarMensagem);
     }
-    btnEnviar.addEventListener("click", mostrarMensagem);
+    
